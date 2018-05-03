d5c7604b81fc1bda5b894edf5baa0cc03b4bc87a
demervalgomes <demerval@supremustecnologia.com.br>
2018-05-02 21:52:38 -0300

    Estruturação exportação dos modulos

---
+++ index.js
@@ -0,0 +1,10 @@
+exports.VerificarEstruturaDB = require('./lib/estrutura/estruturaVerificar');
+
+exports.DAO = require('./lib/DAO');
+exports.GeradorSQL = require('./lib/geradorSql');
+exports.Tabela = require('./lib/tabela');
+
+exports.Fields = require('./lib/estrutura/fields');
+exports.Enums = require('./lib/enums');
+
+exports.AuthMiddleware = require('./lib/middlewares/auth');
