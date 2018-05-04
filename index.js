58a87c8871fafcb536f27658a8316def996b8923
demervalgomes <demerval@supremustecnologia.com.br>
2018-05-03 22:11:22 -0300

    Criando Tabela para consulta

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ exports.VerificarEstruturaDB = require('./lib/estrutura/estruturaVerificar');
 exports.DAO = require('./lib/DAO');
 exports.GeradorSQL = require('./lib/geradorSql');
 exports.Tabela = require('./lib/tabela');
+exports.TabelaConsulta = require('./lib/tabelaConsulta');
 
 exports.Fields = require('./lib/estrutura/fields');
 exports.Enums = require('./lib/enums');
